d10c35d29ace646a77f679cf30e3e5622566285a
Matt Hills <mattintech@gmail.com>
2020-08-11 21:49:50 -0400

    parsing json from gardenAPI

--- indoor/collector/src/collectorService.py
+++ indoor/collector/src/collectorService.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import requests
 
-r = requests.get('http://127.0.0.1:5000/api/solar')
+r = requests.get('http://localhost:5000/api/solar')
 
 ## Get status code
 r.status_code
 
 ## get body
 print(r.text)
+jsonResponse = r.json()
 
+print(jsonResponse['chargingCurrent'])
